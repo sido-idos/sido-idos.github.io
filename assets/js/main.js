0c9e474b0783329946c2efec6db700073ed1b159
sido-idos <118561926+sido-idos@users.noreply.github.com>
2026-03-30 14:50:24 -0300

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const yearEls = document.querySelectorAll("[data-current-year]");
-  const currentYear = new Date().getFullYear();
-
-  yearEls.forEach((el) => {
-    el.textContent = currentYear;
-  });
-});
